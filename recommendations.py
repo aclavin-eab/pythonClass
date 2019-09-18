900f6560d0845d0f25dca660516243e3ee5b9bd4
Andrew Clavin <andrewclavin8@gmail.com>
2019-09-17 23:54:08 -0400

    machine learning tutorials

---
+++ recommendations.py
@@ -0,0 +1,75 @@
+# A dictionary of movie critics and their ratings of a small set of movies
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+    'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0 },
+    'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5,
+        'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5 },
+    'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+        'Superman Returns': 3.5, 'The Night Listener': 4.0},
+    'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+        'Superman Returns': 4.0, 'The Night Listener': 4.5, 'You, Me and Dupree': 2.5 },
+    'Mike Lasalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0,
+        'Superman Returns': 3.0, 'You, Me and Dupree': 2.0, 'The Night Listener': 3.0 },
+    'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just My Luck': 2.0,
+        'Superman Returns': 5.0, 'You, Me and Dupree': 3.5, 'The Night Listener': 3.0 },
+    'Toby': {'Snakes on a Plane': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 1.0},
+    'Toby2': {'Snakes on a Plane': 4.5, 'Superman Returns': 4.0, 'You, Me and Dupree': 1.0}
+}
+
+from math import sqrt
+
+# Returns a distance-based similarity score for p1 and p2
+def sim_distance(prefs,person1,person2):
+    # Get the list of shared_items
+    si={}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+
+    # if they have no ratings in common, return 0
+    if len(si)==0: return 0
+
+    # add up the squares of all differences
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in si])
+
+    return 1/(1+sqrt(sum_of_squares))
+
+def sim_pearson(prefs,p1,p2):
+    # Get the list of mutually rated shared_items
+    si={}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item]=1
+
+    # Find the number of elements
+    n=len(si)
+
+    # if they have no ratings in common, return 0
+    if n==0: return 0
+
+    # Add up all the preferences
+    sum1=sum([prefs[p1][it] for it in si])
+    sum2=sum([prefs[p2][it] for it in si])
+
+    #Sum the squares
+    sum1Sq = sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it],2) for it in si])
+
+    #sum up the products
+    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    #Calculate Pearson score
+    num=pSum-(sum1*sum2/n)
+    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den==0: return 0
+
+    r=num/den
+
+    return r
+
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+    scores=[(similarity(prefs,person,other),other)
+        for other in prefs if other!=person]
+
+    # Sort the list so the highest scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
