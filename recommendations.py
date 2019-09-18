37be8ee5fccb6341bcef9a0d65b30a7f474fe4ef
Andrew Clavin <andrewclavin8@gmail.com>
2019-09-18 00:44:17 -0400

    getRecommendations debugged

--- recommendations.py
+++ recommendations.py
@@ -73,3 +73,37 @@ def topMatches(prefs,person,n=5,similarity=sim_pearson):
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+#Get recommendations for a person by using a weighted average of every other user's rankings
+def getRecommendations(prefs,person,similarity=sim_pearson):
+    totals={}
+    simSums={}
+    for other in prefs:
+        #don't compare to myself
+        if other==person: continue
+        sim=similarity(prefs,person,other)
+
+        #ignore scores of zero or lower
+        if sim<=0: continue
+        for item in prefs[other]:
+
+            #only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item]==0:
+                # Similarity * score
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
+                #Sum of similarities
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+
+    #Create the normalized list
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+    #Return sorted list
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+# recommendations.getRecommendations(recommendations.critics,'Toby',similarity=recommendations.sim_distance)
+
+
